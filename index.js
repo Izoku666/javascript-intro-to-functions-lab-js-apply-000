a65aeb040302b004d415ce91ff1cfa904e06ed0d
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-21 23:40:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function shout(string)
 {
-  if(string ==)
+  if(string === string.toUpperCase())
+  return "";
+  
+  else if(string === string.toLowerCase())
+  return "";
+}
+
+function logShout(string)
+{
+  return console.log(string.toUpperCase());
 }
